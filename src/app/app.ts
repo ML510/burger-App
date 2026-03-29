d9b6677f63b4762aef76bb54a4870bd4433aa780
ML510 <mr.mahesh.303@gmail.com>
2026-03-29 18:36:44 +0530

    Add Font Style And Add AOS

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { AfterViewInit, Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { Home } from "./page/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
-  ngOnInit(): void {
-    initFlowbite();
-  }
-  protected readonly title = signal('BurgersShop');
+export class App implements AfterViewInit {
+  ngAfterViewInit(): void {
+  initFlowbite();
+}
 }
--- src/app/page/home/home.ts
+++ src/app/page/home/home.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { auditTime } from 'rxjs';
+import Typed from 'typed.js';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +9,50 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements AfterViewInit {
 
+  @ViewChild('myVideo') video!: ElementRef;
+  @ViewChild('typingText') typingText!: ElementRef;
+  ngAfterViewInit() {
+    this.playMutedVideo();
+    this.autoText();
+    AOS.init({
+      duration: 1200,
+      once: true,
+      offset: 0,
+    });
+    setTimeout(() => {
+      AOS.refresh();
+    }, 500);
+  }
 
-  
+  autoText(): void {
+    const options = {
+      strings: [`Welcome to the sizzling world of Street Burger, where the burger rulebook is
+                    being rewritten.
+                    Having
+                    flipped the script on traditional burgers, Street Burger has revolutionized the burger scene to
+                    tune
+                    up
+                    a menu that is bound to shift your taste buds into overdrive. Each uniquely crafted burger at
+                    Street
+                    Burger is meant to ignite your taste buds with the thrilling fusion of top-notch ingredients,
+                    daring
+                    creativity, and a dash of Sri Lankan charm.`],
+      typeSpeed: 20,
+      backSpeed: 80,
+      backDelay: 1000,
+      loop: true
+    };
+
+    new Typed(this.typingText.nativeElement, options);
+  }
+
+  playMutedVideo(): void {
+    setTimeout(() => {
+      this.video.nativeElement.muted = true;
+      this.video?.nativeElement?.play();
+    }, 0);
+  }
+
 }
